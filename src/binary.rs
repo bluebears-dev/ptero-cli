ca3a4b9c19acff959f2a2a0207dbb72feb3fa47a
Paweł Górski <dev.baymax42@gmail.com>
2020-11-13 19:34:19 +0100

    Refactored BitIterator implementation * Removed Byte type * Changed the `new` signature to accept raw bytes

--- src/binary.rs
+++ src/binary.rs
@@ -4,9 +4,6 @@ use std::{
     io::{self, BufReader, Read},
 };
 
-/// Type for representing a byte.
-#[derive(Debug, PartialOrd, PartialEq, Clone)]
-pub struct Byte(pub u8);
 /// Type for representing a bit.
 #[derive(Debug, PartialOrd, PartialEq, Clone)]
 pub struct Bit(pub u8);
@@ -15,11 +12,11 @@ pub struct Bit(pub u8);
 struct BinaryPattern(u8);
 
 /// Bit sequence iterator.
-/// It enables user to read [Bits](struct.Bit.html) from any iterator that provides [Bytes](struct.Byte.html).
+/// It enables user to read [Bits](struct.Bit.html) from any iterator that provides bytes as `u8`.
 #[derive(Debug)]
-pub struct BitIterator<I> {
-    iter: I,
-    current_byte: Byte,
+pub struct BitIterator<'a> {
+    bytes: &'a [u8],
+    index: usize,
     fetch_pattern: BinaryPattern,
 }
 
@@ -38,108 +35,104 @@ pub struct BitIterator<I> {
 /// let result_data = read_file_binary("Cargo.toml");
 /// ```
 ///
-pub fn read_file_binary(path: &str) -> io::Result<Vec<Byte>> {
+pub fn read_file_binary(path: &str) -> io::Result<Vec<u8>> {
     let mut binary_data = Vec::<u8>::new();
     let file = File::open(path)?;
     let mut buf_reader = BufReader::new(file);
     buf_reader.read_to_end(&mut binary_data)?;
-    Ok(binary_data.iter().map(|&v| Byte(v)).collect())
+    Ok(binary_data)
 }
+
+const MOST_SIGNIFICANT_BIT_PATTERN: u8 = 0b10000000;
+const CLEARED_PATTERN: u8 = 0b00000000;
 
 impl BinaryPattern {
     fn new() -> BinaryPattern {
-        BinaryPattern(0)
+        BinaryPattern(MOST_SIGNIFICANT_BIT_PATTERN)
     }
 
     fn start(&mut self) {
-        self.0 = 0b10000000;
+        self.0 = MOST_SIGNIFICANT_BIT_PATTERN;
     }
 
     fn is_cleared(&self) -> bool {
-        self.0 == 0
+        self.0 == CLEARED_PATTERN
     }
 
     fn shift(&mut self) {
         self.0 >>= 1;
     }
 
-    fn get(&self, byte: &Byte) -> Bit {
-        match byte.0 & self.0 {
+    fn get(&self, byte: u8) -> Bit {
+        match byte & self.0 {
             0 => Bit(0),
             _ => Bit(1),
         }
     }
 }
 
-impl<I> BitIterator<I> {
-    /// Creates a new iterator for the specified iterator.
-    /// Currently only supports [Byte](struct.Byte.html) type iterator.
+impl<'a> BitIterator<'a> {
+    /// Creates a bit iterator for specified bytes
     ///
     /// **Please note that it begins iteration from the MSB.**
-    /// 
+    ///
     /// # Arguments
     ///
-    /// * `iter` - Iterator of type `Iterator<Item = Byte>`
+    /// * `iter` - Iterator of type `Iterator<Item = u8>`
     ///
     /// # Examples
     ///
     /// ## Returns optional value
     /// ```
-    /// use ptero::binary::{Bit, Byte, BitIterator};
+    /// use ptero::binary::{Bit, BitIterator};
     ///
-    /// let array: Vec<Byte> = vec!(1, 0, 2, 3).iter().map(|&v| Byte(v)).collect();
-    /// let mut iterator = BitIterator::new(array.into_iter());
+    /// let array: Vec<Byte> = vec!(1, 0, 2, 3);
+    /// let mut iterator = BitIterator::new(array);
     ///
     /// let bit = iterator.next().unwrap();
     /// let Bit(value) = bit;
     /// assert_eq!(value, 0);
     /// ```    
-    /// 
+    ///
     /// ## Repeats itself after reaching the end
     /// ```
-    /// use ptero::binary::{Bit, Byte, BitIterator};
+    /// use ptero::binary::{Bit, BitIterator};
     ///
-    /// let array: Vec<Byte> = vec!(0).iter().map(|&v| Byte(v)).collect();
-    /// let mut iterator = BitIterator::new(array.into_iter());
+    /// let array: Vec<Byte> = vec!(0);
+    /// let mut iterator = BitIterator::new(array);
     ///
     /// for v in &mut iterator {
     ///     assert_eq!(v, Bit(0));
-    /// } 
+    /// }
     /// iterator.next();
     /// for v in &mut iterator {
     ///     assert_eq!(v, Bit(0));
     /// }
     /// ```    
-    /// 
     ///
-    pub fn new(iter: I) -> Self {
+    ///
+    pub fn new(vector: &'a [u8]) -> Self {
         // At the first execution we'll fetch the first value and then process it
         BitIterator {
-            iter,
-            current_byte: Byte(0),
+            bytes: vector,
+            index: 0,
             fetch_pattern: BinaryPattern::new(),
         }
     }
 }
 
-impl<I: Iterator<Item = Byte>> Iterator for BitIterator<I> {
+impl<'a> Iterator for BitIterator<'a> {
     type Item = Bit;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.fetch_pattern.is_cleared() {
             self.fetch_pattern.start();
-            self.current_byte = self.iter.next()?;
+            self.index += 1;
         };
-        let bit = self.fetch_pattern.get(&self.current_byte);
+        let byte = *self.bytes.get(self.index)?;
+        let bit = self.fetch_pattern.get(byte);
         self.fetch_pattern.shift();
         Some(bit)
-    }
-}
-
-impl fmt::Display for Byte {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Use `self.number` to refer to each positional data point.
-        write!(f, "{}", self.0)
     }
 }
 
