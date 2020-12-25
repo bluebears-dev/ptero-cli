cddaced4fafd8300086c2f743cc4fc147cc5dbfa
Paweł Górski <dev.baymax42@gmail.com>
2020-12-25 13:06:02 +0100

    Added doctests for text  module

--- src/text.rs
+++ src/text.rs
@@ -26,6 +26,18 @@ impl CoverTextWordIterator {
         }
     }
 
+    /// # Examples
+    ///
+    /// ## Returns the next word without forwarding the iterator
+    /// ```
+    /// use ptero::text::CoverTextWordIterator;
+    ///
+    /// let text = "a b c d e;";
+    /// let iter: CoverTextWordIterator = CoverTextWordIterator::new(&text);
+    ///
+    /// assert_eq!(iter.peek(), Some("a".to_owned()));
+    /// assert_eq!(iter.peek(), Some("a".to_owned()));
+    /// ```
     pub fn peek(&self) -> Option<String> {
         self.words
             .get(self.word_index)
@@ -36,6 +48,31 @@ impl CoverTextWordIterator {
 impl Iterator for CoverTextWordIterator {
     type Item = String;
 
+    /// # Examples
+    ///
+    /// ## Returns the next word
+    /// ```
+    /// use ptero::text::CoverTextWordIterator;
+    ///
+    /// let text = "a b c";
+    /// let mut iter: CoverTextWordIterator = CoverTextWordIterator::new(&text);
+    ///
+    /// assert_eq!(iter.next(), Some("a".to_owned()));
+    /// assert_eq!(iter.next(), Some("b".to_owned()));
+    /// ```
+    /// ## Returns `None` when iterator has traversed all the words and does not repeat
+    /// ```
+    /// use ptero::text::CoverTextWordIterator;
+    ///
+    /// let text = "a b c";
+    /// let mut iter: CoverTextWordIterator = CoverTextWordIterator::new(&text);
+    ///
+    /// assert_eq!(iter.next(), Some("a".to_owned()));
+    /// assert_eq!(iter.next(), Some("b".to_owned()));
+    /// assert_eq!(iter.next(), Some("c".to_owned()));
+    /// assert_eq!(iter.next(), None);
+    /// assert_eq!(iter.next(), None);
+    /// ```
     fn next(&mut self) -> Option<Self::Item> {
         let word = self.peek()?;
         self.word_index += 1;
@@ -58,6 +95,29 @@ impl CoverTextLineIterator {
 impl Iterator for CoverTextLineIterator {
     type Item = String;
 
+    /// # Examples
+    ///
+    /// ## Returns the next line
+    /// ```
+    /// use ptero::text::CoverTextLineIterator;
+    ///
+    /// let text = "a b c";
+    /// let mut iter: CoverTextLineIterator = CoverTextLineIterator::new(&text);
+    ///
+    /// assert_eq!(iter.next(), Some("a b c".to_owned()));
+    /// ```
+    /// ## Returns `None` when traversed all the lines and does not repeat
+    /// ```
+    /// use ptero::text::CoverTextLineIterator;
+    ///
+    /// let text = "a b c\na";
+    /// let mut iter: CoverTextLineIterator = CoverTextLineIterator::new(&text);
+    ///
+    /// assert_eq!(iter.next(), Some("a b c".to_owned()));
+    /// assert_eq!(iter.next(), Some("a".to_owned()));
+    /// assert_eq!(iter.next(), None);
+    /// assert_eq!(iter.next(), None);
+    /// ```
     fn next(&mut self) -> Option<Self::Item> {
         let line = self.lines.get(self.line_index).map(|x| x.to_owned())?;
         self.line_index += 1;
